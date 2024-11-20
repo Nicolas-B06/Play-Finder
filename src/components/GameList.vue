1954883912005497ec8c6b8f2be1a34d101f7ca4
Nicolas-B06 <bernardnicolas06600@gmail.com>
2024-11-20 12:00:01 +0100

    add  games logos and visual adjustments

--- src/components/GameList.vue
+++ src/components/GameList.vue
@@ -1,24 +1,48 @@
 <template>
-  <div class="w-full max-w-md mx-auto">
+  <div class="w-full mx-auto">
     <h2 class="text-2xl font-bold text-center text-secondary mb-4">
       Liste des jeux
     </h2>
 
     <div class="overflow-y-auto" style="max-height: calc(2.5rem * 15)">
       <table
-        class="w-full border-collapse border border-secondary rounded-lg shadow-md"
+        class="w-full border-secondary rounded-lg shadow-md"
       >
         <thead>
-          <tr class="sticky top-0 bg-highlight text-secondary">
-            <th class="px-4 py-2 text-center">Jeux</th>
+          <tr class="sticky top-0 bg-highlight text-secondary ">
+            <th class="px-4 py-2 text-center">Logo</th>
+            <th class="px-4 py-2 text-center">Jeu</th>
+            <th class="px-4 py-2 text-center">Activer</th>
             <th class="px-4 py-2 text-center">Actions</th>
           </tr>
         </thead>
         <tbody>
           <tr v-for="(game, index) in games" :key="index" class="bg-white">
-            <td class="border border-secondary px-4 py-2 text-primary">
-              {{ game }}
+            
+            <td class="border border-secondary px-4 py-2 text-center">
+              <img
+                v-if="game.iconUrl"
+                :src="game.iconUrl"
+                alt="Logo du jeu"
+                class="w-12 h-12 object-cover rounded-md"
+              />
             </td>
+
+          
+            <td class="border border-secondary px-4 py-2 text-primary">
+              {{ game.name }}
+            </td>
+
+            <td class="border border-secondary px-4 py-2 text-center">
+              <button
+                @click="toggleGameActivation(index)"
+                :class="game.active ? 'bg-green-500' : 'bg-red-500'"
+                class="px-2 py-1 text-white font-semibold rounded-lg shadow"
+              >
+                {{ game.active ? 'Activé' : 'Désactivé' }}
+              </button>
+            </td>
+
             <td class="border border-secondary px-4 py-2 text-center">
               <button
                 @click="removeGame(index)"
@@ -31,7 +55,6 @@
         </tbody>
       </table>
     </div>
-
     <div class="mt-4 flex items-center">
       <input
         v-model="newGame"
@@ -52,12 +75,12 @@
     >
       Importer mes jeux Steam
     </button>
-    <SteamModal
-      v-if="showSteamModal"
-      @close="closeSteamModal"
-      @import="importSteamGames"
-    />
   </div>
+  <SteamModal
+    v-if="showSteamModal"
+    @close="closeSteamModal"
+    @import="importSteamGames"
+  />
 </template>
 
 <script>
@@ -65,6 +88,7 @@ import SteamModal from './SteamModal.vue';
 
 export default {
   name: 'GameList',
+  emits: ['update-games'],
   components: {
     SteamModal,
   },
@@ -84,14 +108,22 @@ export default {
   methods: {
     addGame() {
       if (this.newGame.trim() !== '') {
-        this.games.push(this.newGame.trim());
+        this.games.push({
+          name: this.newGame.trim(),
+          iconUrl: null,
+          active: true,
+        });
         this.newGame = '';
-        this.$emit('update-games', this.games);
+        this.$emit('update-games', this.games); 
       }
+    },
+    toggleGameActivation(index) {
+      this.games[index].active = !this.games[index].active;
+      this.$emit('update-games', this.games); 
     },
     removeGame(index) {
       this.games.splice(index, 1);
-      this.$emit('update-games', this.games);
+      this.$emit('update-games', this.games); 
     },
     openSteamModal() {
       console.log('Méthode openSteamModal appelée');
@@ -103,21 +135,27 @@ export default {
     async importSteamGames(steamId) {
       try {
         const response = await fetch(
-          `${process.env.VUE_APP_API_BASE_URL}/IPlayerService/GetOwnedGames/v0001/?key=${process.env.VUE_APP_STEAM_API_KEY}&steamid=${steamId}&include_appinfo=true&format=json`);
+          `/api/IPlayerService/GetOwnedGames/v1/?key=${process.env.VUE_APP_STEAM_API_KEY}&steamid=${steamId}&include_appinfo=true`
+        );
         const data = await response.json();
 
         if (data.response && data.response.games) {
-          const importedGames = data.response.games.map((game) => game.name);
-          this.games = [...new Set([...this.games, ...importedGames])];
+          const importedGames = data.response.games.map((game) => ({
+            name: game.name,
+            iconUrl: game.img_icon_url
+              ? `http://media.steampowered.com/steamcommunity/public/images/apps/${game.appid}/${game.img_icon_url}.jpg`
+              : null,
+            active: true, 
+          }));
+          this.games = [...this.games, ...importedGames];
           this.$emit('update-games', this.games);
-          alert(`${importedGames.length} jeux importés avec succès !`);
         } else {
           alert('Aucun jeu trouvé ou Steam ID incorrect.');
         }
       } catch (error) {
         console.error('Erreur lors de l’importation des jeux Steam:', error);
         alert(
-          'Une erreur est survenue lors de l’importation. Veuillez vérifier votre Steam ID ou réessayer plus tard.'
+          'Erreur lors de l’importation des jeux Steam. Veuillez réessayer.'
         );
       } finally {
         this.closeSteamModal();
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,50 +3,76 @@
     <h1 class="text-4xl font-bold text-secondary mb-24 text-center">
       Play<span class="text-accent">Finder</span>
     </h1>
-
-    <div class="flex flex-row items-start space-x-8 w-full max-w-4xl justify-center">
-      <div class="w-1/2 max-w-md border border-secondary rounded-lg shadow-md p-4 bg-highlight">
+    <div
+      class="flex flex-row items-start space-x-8 w-full max-w-4xl justify-center"
+    >
+      <div
+        class="w-1/2 max-w-md border border-secondary rounded-lg shadow-md p-4 bg-highlight"
+      >
         <GameList :initialGames="games" @update-games="updateGames" />
       </div>
-
       <div class="flex flex-col items-center w-1/2 max-w-md">
-        <div
-          ref="roller"
-          class="relative w-64 h-16 overflow-hidden border-4 border-secondary rounded-lg shadow-md bg-highlight"
-        >
+        <div class="relative w-64">
           <div
-            class="flex flex-col items-center transition-transform duration-100 ease-linear"
-            :style="{
-              transform: `translateY(-${position}px)`,
-              transition: isRolling
-                ? 'transform 0.1s linear'
-                : 'transform 1s ease-out',
-            }"
+            class="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-full"
+          >
+            <div class="w-4 h-4 bg-accent transform rotate-45"></div>
+          </div>
+          <div
+            ref="roller"
+            class="relative w-64 h-16 overflow-hidden border-highlight rounded-lg shadow-md bg-white"
           >
             <div
-              v-for="(game, index) in repeatedGames"
-              :key="index"
-              class="text-lg font-semibold text-secondary h-16 flex items-center justify-center"
+              class="flex flex-col items-center transition-transform duration-100 ease-linear"
+              :style="{
+                transform: `translateY(-${position}px)`,
+                transition: isRolling
+                  ? 'transform 0.1s linear'
+                  : 'transform 1s ease-out',
+              }"
             >
-              {{ game }}
+              <div
+                v-for="(game, index) in repeatedGames"
+                :key="index"
+                class="h-16 flex items-center justify-center"
+              >
+                <img
+                  v-if="game.iconUrl"
+                  :src="game.iconUrl"
+                  alt="Logo du jeu"
+                  class="w-12 h-12 object-cover"
+                />
+
+                <span v-else class="text-secondary font-bold text-lg">{{
+                  game.name
+                }}</span>
+              </div>
             </div>
           </div>
         </div>
         <button
           @click="startRoll"
+          :disabled="isButtonDisabled"
           class="mt-4 px-6 py-2 bg-accent text-secondary font-semibold rounded-lg shadow hover:bg-green-400 transition duration-300"
         >
           Lancer
         </button>
-        <p v-if="selectedGame" class="mt-4 text-lg text-secondary">
-          🎲 Jeu sélectionné : <span class="font-bold text-accent">{{ selectedGame }}</span>
-        </p>
+        <div class="mt-4 flex flex-col items-center">
+          <img
+            v-if="selectedGame && selectedGame.iconUrl"
+            :src="selectedGame.iconUrl"
+            alt="Image du jeu"
+            class="mt-2 mb-2 w-32 h-32 object-cover rounded-lg shadow-md"
+          />
+          <p v-if="selectedGame" class="text-lg text-secondary">
+            🎲 Jeu sélectionné :
+            <span class="font-bold text-accent">{{ selectedGame.name }}</span>
+          </p>
+        </div>
       </div>
     </div>
-
   </div>
 </template>
-
 
 <script>
 import GameList from '../components/GameList.vue';
@@ -60,24 +86,34 @@ export default {
       position: 0,
       isRolling: false,
       rollInterval: null,
+      isButtonDisabled: false,
     };
   },
   computed: {
     repeatedGames() {
-      return [...this.games, ...this.games, ...this.games];
+      return this.games.filter((game) => game.active).flatMap((game) => [game]);
     },
   },
   methods: {
     startRoll() {
       if (this.isRolling) return;
 
+      const activeGames = this.games.filter((game) => game.active);
+
+      if (activeGames.length === 0) {
+        alert('Aucun jeu activé pour la roulette.');
+        return;
+      }
+
+      this.isButtonDisabled = true;
       this.isRolling = true;
       this.position = 0;
       this.selectedGame = null;
 
+      const rowHeight = 64;
       this.rollInterval = setInterval(() => {
-        this.position += 64;
-        if (this.position >= this.repeatedGames.length * 64) {
+        this.position += rowHeight;
+        if (this.position >= this.repeatedGames.length * rowHeight) {
           this.position = 0;
         }
       }, 50);
@@ -86,18 +122,18 @@ export default {
         clearInterval(this.rollInterval);
         this.isRolling = false;
 
-        const extraRotations = Math.floor(Math.random() * 5 + 5);
-        const randomIndex = Math.floor(Math.random() * this.games.length);
-        const randomOffset = randomIndex * 64;
-
-        this.position = extraRotations * this.games.length * 64 + randomOffset;
-        this.selectedGame = this.games[randomIndex];
+        const randomIndex = Math.floor(Math.random() * activeGames.length);
+        const randomOffset = randomIndex * rowHeight;
 
         this.position = randomOffset;
-      }, 4000);
+        this.selectedGame = activeGames[randomIndex];
+
+        this.isButtonDisabled = false;
+      }, 6000);
     },
+
     updateGames(updatedGames) {
-      this.games = [...updatedGames];
+      this.games = updatedGames;
     },
   },
   components: {
@@ -118,5 +154,43 @@ export default {
 }
 .bg-brightYellow {
   background-color: #ffeb3b;
+}
+.bg-accent {
+  background-color: #7cfb47;
+}
+
+.border-highlight {
+  border: 4px solid #f5f5f5;
+  box-shadow:
+    0 0 15px #7cfb47,
+    0 0 30px #7cfb47;
+  border-radius: 8px;
+}
+button:disabled {
+  opacity: 0.5;
+  cursor: not-allowed;
+}
+
+@keyframes pulse {
+  from {
+    transform: scale(1);
+    opacity: 0.8;
+  }
+  to {
+    transform: scale(1.1);
+    opacity: 1;
+  }
+}
+
+@keyframes bg-move {
+  from {
+    background-position: 0% 50%;
+  }
+  to {
+    background-position: 100% 50%;
+  }
+}
+* {
+  box-sizing: content-box !important;
 }
 </style>
